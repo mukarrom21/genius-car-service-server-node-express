c97fb7cd0e2c4bf27d9fe172b52f6f628cc99261
Mukarrom <mukarrom21@yahoo.com>
2022-05-05 05:28:09 +0600

    some update

--- __server_deploy.js
+++ __server_deploy.js
@@ -10,4 +10,15 @@
  * For each one project
  * -------------------------------
  * 1. heroku create
+ * 2. make sure you: git add . commit push
+ * 3. git push heroku main
+ * 4. go to Heroku Dashboard > current project > settings > reveal config vars
+ * 5. copy past your from your .env file
+ * 6. make sure you have whitelisted all ip address to access mongodb 
+ * -------------------------------------------
+ * update server with new changes
+ * -------------------------------------------
+ * 1. Make changes
+ * 2. make sure you: git add . commit push
+ * 3. git push heroku main
  */
